99c6eae6701021715be3fffb19385f41fb9f2c48
the-sides <sides798@gmail.com>
2019-08-09 00:45:50 -0400

    tracking files

---
+++ server.js
@@ -0,0 +1,38 @@
+var http = require("http");
+const express = require("express");
+const path = require("path");
+// const pug = require("pug");
+const sass = require("node-sass");
+const port = 3000;
+
+let app = express();
+
+// Set views
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+// Set public director
+app.use(express.static(path.join(__dirname, "public")));
+// Set styles
+// app.use(
+//   sass.middleware({
+//       src: __dirname + '/sass', //where the sass files are 
+//       dest: __dirname + '/public/main.css', //where css should go
+//       debug: true // obvious
+//   })
+// );
+
+// Basic route
+let router = express.Router();
+app.use(
+  "/",
+  router.get("/", function(req, res) {
+    res.render("home");
+  })
+);
+
+app.set("port", port);
+
+//create a server object:
+let server = http.createServer(app);
+server.listen(port); //the server object listens on port 8080
