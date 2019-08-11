da8a68d1732857e50013ce38c4b3f1587ec97a93
the-sides <sides798@gmail.com>
2019-08-11 02:32:56 -0400

    looping video sources

--- src/scripts/client.js
+++ src/scripts/client.js
@@ -1,1 +1,32 @@
-window.alert('wow')
+const vid = document.querySelector('video')
+const vidSrc = vid.querySelector('source')
+const bRoll = document.querySelector('#bRoll');
+// const vid2 = document.querySelector('video')
+
+// vidSrc.setAttribute('src', path(0));
+// vid.load();
+// vid.play();
+let ticker = 0;
+updateSrc(vid, vidSrc, path(ticker++))
+vid.addEventListener('ended', ()=>{
+    console.log('ended')
+    updateSrc(vid, vidSrc, path(ticker++))
+})
+function path(ind){
+    const videoSources = [
+        'clashes',
+        'coffeeShop',
+        'floating',
+        'neistat',
+        'poeticAnime',
+        'animeRap'
+    ]
+    console.log(`./images/${videoSources[ind % videoSources.length]}.webm`)
+    return `./images/${videoSources[ind % videoSources.length]}.webm`
+}
+function updateSrc(videoElm, sourceElm, srcPath){
+    sourceElm.setAttribute('src', srcPath);
+    videoElm.load();
+    videoElm.play();
+
+}
